5be32695c63cd22ecf9328051f14d49052a26896
Waqar Ali <waqar.ali911@gmail.com>
2019-12-17 23:25:13 +0100

    Updates

---
+++ precache-manifest.e8ae9e14a5b3a698d8c21957b7bc8286.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "851329b00a90a10fc0dc373bbf9317ac",
+    "url": "/currency-exchange/index.html"
+  },
+  {
+    "revision": "4f3897baa3aa903db90d",
+    "url": "/currency-exchange/static/css/2.764ccc25.chunk.css"
+  },
+  {
+    "revision": "c8bf98964647f8e5974a",
+    "url": "/currency-exchange/static/css/main.88824ec2.chunk.css"
+  },
+  {
+    "revision": "4f3897baa3aa903db90d",
+    "url": "/currency-exchange/static/js/2.a8173efc.chunk.js"
+  },
+  {
+    "revision": "e928fe768baa9832b5bc57eae021f30c",
+    "url": "/currency-exchange/static/js/2.a8173efc.chunk.js.LICENSE"
+  },
+  {
+    "revision": "c8bf98964647f8e5974a",
+    "url": "/currency-exchange/static/js/main.dd14cc92.chunk.js"
+  },
+  {
+    "revision": "df84456e48f18bc9aaf1",
+    "url": "/currency-exchange/static/js/runtime-main.ee39b6f7.js"
+  }
+]);
