9b76f34ce8784ae4968d9e53f4d989b1d9814ae4
Waqar Ali <waqar.ali911@gmail.com>
2020-01-02 15:53:56 +0100

    Updates

---
+++ precache-manifest.0a0acc7e7d6432dc1c321df057995e96.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f455186ffe5753ce5de48501e19f0ff4",
+    "url": "/currency-exchange/index.html"
+  },
+  {
+    "revision": "28f945fbe4662b2825bd",
+    "url": "/currency-exchange/static/css/2.764ccc25.chunk.css"
+  },
+  {
+    "revision": "87fe7b0f5438fc381482",
+    "url": "/currency-exchange/static/css/main.96b8a6e7.chunk.css"
+  },
+  {
+    "revision": "28f945fbe4662b2825bd",
+    "url": "/currency-exchange/static/js/2.fee03f14.chunk.js"
+  },
+  {
+    "revision": "e928fe768baa9832b5bc57eae021f30c",
+    "url": "/currency-exchange/static/js/2.fee03f14.chunk.js.LICENSE"
+  },
+  {
+    "revision": "87fe7b0f5438fc381482",
+    "url": "/currency-exchange/static/js/main.aa75a2a6.chunk.js"
+  },
+  {
+    "revision": "df84456e48f18bc9aaf1",
+    "url": "/currency-exchange/static/js/runtime-main.ee39b6f7.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.6f90395957953025907bedf3d33ad9d7.js"
+  "/currency-exchange/precache-manifest.0a0acc7e7d6432dc1c321df057995e96.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/currency-exchange/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
